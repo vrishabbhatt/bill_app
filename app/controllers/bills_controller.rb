33768ba333eec5a7b7589411ae2c1ffb6788ed11
vrishab <vrishab2494@gmail.com>
2016-09-22 11:33:25 +0530

    user will not be authenticated for show action of bills

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,5 +1,5 @@
 class BillsController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user! , :except => [:show]
 	
 	def index
 		@bills = Bill.created_by(current_user.id)
